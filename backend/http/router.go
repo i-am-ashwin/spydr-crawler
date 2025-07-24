395b943bcf4249ecb4f12fd7a7a63dea11276b92
i-am-ashwin <aswin.harikumar@yahoo.com>
2025-07-24 18:08:51 +0200

    feat: add auth middleware

--- backend/http/router.go
+++ backend/http/router.go
@@ -3,6 +3,7 @@ package http
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/i-am-ashwin/spydr-crawler/backend/middleware"
 	"github.com/i-am-ashwin/spydr-crawler/backend/worker"
 	"gorm.io/gorm"
 )
@@ -10,22 +11,26 @@ import (
 func SetupRouter(db *gorm.DB, pool *worker.WorkerPool) *gin.Engine {
 	r := gin.Default()
 
-	// Configure CORS
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://localhost:3000"}
-	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Cache-Control"}
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Cache-Control", "Authorization"}
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
 	r.Use(cors.New(config))
 
 	r.GET("/health", func(c *gin.Context) { c.JSON(200, gin.H{"ok": true}) })
-	handlers := &Handlers{DB: db, WorkerPool: pool}
+	r.POST("/api/auth/login", middleware.Login)
 
-	r.POST("/api/crawl", handlers.CreateCrawlJob)
-	r.GET("/api/crawl/:id", handlers.GetCrawlJob)
-	r.GET("/api/crawl/list", handlers.ListCrawlJobs)
-	r.GET("/api/crawl/updates", handlers.CrawlJobUpdatesSSE)
-	r.POST("/api/crawl/:id/stop", handlers.StopCrawlJob)
-	r.DELETE("/api/crawl/:id", handlers.DeleteCrawlJob)
+	handlers := &Handlers{DB: db, WorkerPool: pool}
+	protected := r.Group("/api")
+	protected.Use(middleware.JWTAuthMiddleware())
+	{
+		protected.POST("/crawl", handlers.CreateCrawlJob)
+		protected.GET("/crawl/:id", handlers.GetCrawlJob)
+		protected.GET("/crawl/list", handlers.ListCrawlJobs)
+		protected.GET("/crawl/updates", handlers.CrawlJobUpdatesSSE)
+		protected.POST("/crawl/:id/stop", handlers.StopCrawlJob)
+		protected.DELETE("/crawl/:id", handlers.DeleteCrawlJob)
+	}
 
 	return r
 }
